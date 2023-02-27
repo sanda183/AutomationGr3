0e547c5c39eb5cdc30a7201ac5da37846c00dec1
marin <marincassanda@yahoo.com>
2023-02-27 20:46:19 +0200

    The first base

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasePage {
+private static final Logger LOG = LoggerFactory.getLogger(BasePage.class);
+public static WebDriver driver;
+
+
+public static void setUp(){
+    LOG.info ("Start test");
+    System.setProperty("webdriver.chrome.driver", "C://Webdriverchromedriver110.exe");
+    driver = new ChromeDriver();
+    String url = "https://demo.automationtesting.in/Index.html";
+    driver.get(url);
+
+    LOG.info("Open browser maximize");
+    driver.manage().window().maximize();
+}
+
+    public static void tearDown() {
+
+
+    LOG.info("Close the browser");
+    driver.quit();
+    }
+
+
+
+}
+
